3842b2f45192da60f35d47f0cb4263ef8910985a
cerebropine <imtiazkhan_ds@outlook.com>
2021-07-08 09:39:48 +0530

    https://leetcode.com/problems/rotate-image/submissions/

---
+++ Arrays/rotate_matrix.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int temp;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
+            }
+       }
+    }
+};
+
+int main()
+{
+    vector<vector<int>> arr={{1,2,3},{4,5,6},{7,8,9}};
+    Solution sol;
+    sol.rotate(arr);
+    return 0;
+}
