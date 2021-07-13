76558622c6de4175dd98e97d4a1d627423e71dd2
cerebropine <imtiazkhan_ds@outlook.com>
2021-07-13 13:17:51 +0530

    https://practice.geeksforgeeks.org/problems/row-with-max-1s0023/1#

--- Arrays/row_max_ones.cpp
+++ Arrays/row_max_ones.cpp
@@ -5,30 +5,19 @@ using namespace std;
 class Solution{
 public:
 	int rowWithMax1s(vector<vector<int>> arr, int n, int m) {
-		int i,j;int ans;
-		i=0;j=m-1;ans=-1;
-		while(i<=n-1 && j>0){
-			if(arr[i][j]==0){
-				i=i+1;	
-			}
-			else{
-				if(arr[i][j]==arr[i][j-1]) {
-					j=j-1;
-					ans=i;
-				}
-				else{
-					i=i+1;
-					
-				}				
+		int i,j;int ans=0,ans_row;
+		i=0;j=m-1;ans_row=-1;
+		while(i<=n-1 && j>=0){
+			if(arr[i][j]==0) i++;
+			else j--;
+			
+			int count_of_ones=m-1-j;
+			if(count_of_ones>ans){
+				ans=count_of_ones;
+				ans_row=i;
 			}
 		}
-
-		if(j==m-1){
-			for(int k=0;k<n;k++){
-				if(arr[k][j]==1) return k;
-			}
-		}
-	 	return ans;
+	 	return ans_row;
 	}
 
 };
